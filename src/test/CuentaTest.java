3816a8d0471f343abd0c9166530044847d0624d8
Alejandro <alejandrominmar@gmail.com>
2025-10-13 17:06:10 +0200

    pruebas completadas

--- src/test/CuentaTest.java
+++ src/test/CuentaTest.java
@@ -12,11 +12,13 @@ import pkg.Cuenta;
 
 class CuentaTest {
 	
-	static Cuenta cuenta;
-
+	static Cuenta cuenta12345;
+	static Cuenta cuenta67890;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		cuenta = new Cuenta(0);
+		cuenta12345 = new Cuenta(0);
+		cuenta67890 = new Cuenta(0);
 	}
 
 	@AfterAll
@@ -25,14 +27,15 @@ class CuentaTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		cuenta.setSaldo(0.0);
+		cuenta12345.setSaldo(50.0);
+		cuenta67890.setSaldo(0.0);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
-	@Test
+	/*@Test
 	void testIngresar() {
 		cuenta.ingresar(500.0);
 		assertEquals(500, cuenta.getSaldo());
@@ -42,5 +45,40 @@ class CuentaTest {
 	void testRetirar() {
 		cuenta.retirar(500.0);
 		assertEquals(-500, cuenta.getSaldo());
+	}*/
+	
+	@Test
+	void test0014() {	
+		cuenta12345.retirar(200.0);
+		assertEquals(-150, cuenta12345.getSaldo());
+		
+		
+		cuenta12345.ingresar(100.0);
+		assertEquals(-50, cuenta12345.getSaldo());
+		
+		
+		cuenta12345.retirar(200.0);
+		assertEquals(-250, cuenta12345.getSaldo());
+		
+		//cuenta67890
+		
+		cuenta67890.retirar(350.0);
+		assertEquals(-350, cuenta67890.getSaldo());
+		
+		
+		cuenta67890.retirar(200.0);
+		assertEquals(-350, cuenta67890.getSaldo());
+		
+		
+		cuenta67890.retirar(150.0);
+		assertEquals(-500, cuenta67890.getSaldo());
+		
+		cuenta67890.ingresar(50.0);
+		assertEquals(-450, cuenta67890.getSaldo());
+		
+		cuenta67890.retirar(100.0);
+		assertEquals(-450, cuenta67890.getSaldo());
 	}
+	
+	
 }
